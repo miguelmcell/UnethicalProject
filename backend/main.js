3c0c1017c0cc2bc5e24dd027cc09d1f940bcd003
miguelmcell <miguelmcell@utexas.edu>
2019-12-02 20:42:24 -0600

    added post endpoint

--- backend/main.js
+++ backend/main.js
@@ -24,22 +24,20 @@ app.get('/getAccount/:accountId', function (req, res) {
       if(error) throw error;
       res.send(results);
     });
-
-
-    // connection.getConnection(function (err, connection) {
-    // if (err) throw err;
-    // let accountId = req.params.accountId;
-    // // Executing the MySQL query (select all data from the 'users' table).
-    // let sqlQuery = 'SELECT * FROM users WHERE account_id ="' + accountId + '"';
-    // console.log(sqlQuery);
-    // connection.query(sqlQuery, function (error, results, fields) {
-    //   // If some error occurs, we throw an error.
-    //   if (error) throw error;
-    //
-    //   // Getting the 'response' from the database and sending it to our route. This is were the data is.
-    //   res.send(results)
-    // });
-  // });
+});
+app.post('/createAccount/:accountId', function (req, res) {
+    // Connecting to the database
+    console.log("createaccount hit");
+    let accountId = req.params.accountId;
+    // Executing the MySQL query (select all data from the 'users' table).
+    // INSERT INTO users(account_id,plain_text_password,name) VALUES ('458151934522', 'Money123', 'Tom');
+    let sqlQuery = '' + accountId;
+    console.log("now querying");
+    connection.query(sqlQuery, function(error,results,fields){
+      if(error) throw error;
+      console.log("it worked");
+      res.send("Account added!");
+    });
 });
 
 // Starting our server.
