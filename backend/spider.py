71f5563b9be6bb455d7f13627315d14898a3c7e4
Anish Bose <anishbose26@gmail.com>
2019-12-03 15:31:20 -0600

    Update spider to grab js files and output to txt

--- backend/spider.py
+++ backend/spider.py
@@ -15,6 +15,7 @@ def spider(start_url, max_pages):
     q.put(start)
     # Set of strings
     crawled_urls = set()
+    crawled_js_urls = set()
 
     while not q.empty():
         link = q.get()
@@ -27,16 +28,37 @@ def spider(start_url, max_pages):
             if(len(crawled_urls) >= max_pages):
                 print('reached max')
                 break
+            # Crawl href links
             for url in soup.find_all('a', href=True):
                 if url['href'].startswith('http://') or url['href'].startswith('https://'):
                     q.put(Link(url['href']))
                 else:
                     q.put(Link(link.base_url + url['href']))
+            # Crawl .js links
+            for url in soup.find_all('script', src=True):
+                if url['src'].startswith('http://') or url['src'].startswith('https://'):
+                    crawled_js_urls.add(url['src'])
+                else:
+                    crawled_js_urls.add(link.base_url + url['src'])
         except:
             pass
             #print('failed:')
             #print(link.base_url)
+
+    writeToFiles(crawled_urls, crawled_js_urls)
     print(crawled_urls)
+    print(crawled_js_urls)
+
+def writeToFiles(urls, js_urls):
+    url_txt = open("spider_urls.txt","w")
+    for line in urls:
+        url_txt.write(line + '\n')
+    url_txt.close()
+
+    js_url_txt = open("spider_js_urls.txt", "w")
+    for line in js_urls:
+        js_url_txt.write(line + '\n')
+    js_url_txt.close()
 
 
 
@@ -44,6 +66,6 @@ def spider(start_url, max_pages):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--url", help="the url to begin crawling at", default="https://www.cs.utexas.edu/people/faculty-researchers/chris-prosise")
-    parser.add_argument("--pages", help="the ,ax number of pages to crawl", type=int, default=50)
+    parser.add_argument("--pages", help="the max number of pages to crawl", type=int, default=50)
     args = parser.parse_args()
     spider(args.url, args.pages)
