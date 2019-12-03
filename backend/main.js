22aa17ea986441035c83bd5bfa2a895f5a8000a0
miguelmcell <miguelmcell@utexas.edu>
2019-12-02 20:03:27 -0600

    fixed hanging issue

--- backend/main.js
+++ backend/main.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 var cors = require('cors');
 
-const connection = mysql.createPool({
+const connection = mysql.createConnection({
   host     : '35.193.109.77',
   user     : 'root',
   password : '123456789',
@@ -12,24 +12,34 @@ const connection = mysql.createPool({
 
 const app = express();
 app.use(cors());
+connection.connect();
 // 253153934829
 // Creating a GET route that returns data from the 'users' table.
 app.get('/getAccount/:accountId', function (req, res) {
-    // Connecting to the database.
-    connection.getConnection(function (err, connection) {
-    if (err) throw err;
+    // Connecting to the database
     let accountId = req.params.accountId;
     // Executing the MySQL query (select all data from the 'users' table).
     let sqlQuery = 'SELECT * FROM users WHERE account_id ="' + accountId + '"';
-    console.log(sqlQuery);
-    connection.query(sqlQuery, function (error, results, fields) {
-      // If some error occurs, we throw an error.
-      if (error) throw error;
-
-      // Getting the 'response' from the database and sending it to our route. This is were the data is.
-      res.send(results)
+    connection.query(sqlQuery, function(error,results,fields){
+      if(error) throw error;
+      res.send(results);
     });
-  });
+
+
+    // connection.getConnection(function (err, connection) {
+    // if (err) throw err;
+    // let accountId = req.params.accountId;
+    // // Executing the MySQL query (select all data from the 'users' table).
+    // let sqlQuery = 'SELECT * FROM users WHERE account_id ="' + accountId + '"';
+    // console.log(sqlQuery);
+    // connection.query(sqlQuery, function (error, results, fields) {
+    //   // If some error occurs, we throw an error.
+    //   if (error) throw error;
+    //
+    //   // Getting the 'response' from the database and sending it to our route. This is were the data is.
+    //   res.send(results)
+    // });
+  // });
 });
 
 // Starting our server.
